98ac132b0877ac73f228e70d7988496523684b82
woo3145 <lcwoo3145@gmail.com>
2023-01-31 21:48:49 +0900

    css 규칙 설정, @mixin - font-size 설정

--- src/app/_common/header.tsx
+++ src/app/_common/header.tsx
@@ -1,26 +1,36 @@
 import Link from 'next/link';
 import styles from './header.module.scss';
 
+const Logo = () => {
+  return (
+    <Link href="/" className={styles.logo}>
+      <b>Woo3145</b> Community
+    </Link>
+  );
+};
+const SignupButton = () => {
+  return (
+    <Link href={'/login'} className={styles.signup_button}>
+      회원가입/로그인
+    </Link>
+  );
+};
+
 export const Header = () => {
+  const isLogin = false;
   return (
     <>
       <div className={styles.wrapper}>
         <div className={styles.container}>
           <nav className={styles.nav}>
-            <div className={styles.nav_left}>
-              <Link href="/" className={styles.logo}>
-                <b>Woo3145</b> Community
-              </Link>
+            <div>
+              <Logo />
             </div>
-            <div className={styles.nav_right}>
-              <Link href={'/login'} className={styles.signup_button}>
-                회원가입/로그인
-              </Link>
-            </div>
+            <div>{isLogin ? null : <SignupButton />}</div>
           </nav>
         </div>
       </div>
-      <div className={styles.padding}></div>
+      <div className={styles.paddingBox}></div>
     </>
   );
 };
