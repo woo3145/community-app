f4ce0c8e8c8e49cfa9c525c1fc0b034591f21d9f
woo3145 <lcwoo3145@gmail.com>
2023-02-06 22:35:58 +0900

    Write page 작업

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,12 +1,22 @@
-import { Inter } from '@next/font/google';
 import styles from './page.module.scss';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Write() {
   return (
-    <main className={styles.main}>
-      <h2 className={inter.className}>Write</h2>
+    <main className={styles.wrapper}>
+      <div className={styles.writeHeaderWrapper}>
+        <div className={styles.writeHeader}>
+          <button>등록하기</button>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.tagSection}></div>
+        <div className={styles.title}>
+          <input type="text" placeholder="제목을 입력해주세요." />
+        </div>
+        <div className={styles.content}>
+          <textarea name="" id="" placeholder="내용을 작성해주세요."></textarea>
+        </div>
+      </div>
     </main>
   );
 }
