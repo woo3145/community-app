7aaee46a1a9df7720970af0545c56844aa4cb449
woo3145 <lcwoo3145@gmail.com>
2023-02-17 11:54:59 +0900

    Signin, Signup 로직 작성

--- src/app/login/components/emailLogin.tsx
+++ src/app/login/components/emailLogin.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { IoChevronBackOutline } from 'react-icons/io5';
 import styles from './signup.module.scss';
 
@@ -6,9 +8,44 @@ interface Props {
   email: string;
 }
 
+interface FormData {
+  password: string;
+}
+
 export const EmailLogin = ({ onPrevPage, email }: Props) => {
-  const onClick = () => {
-    // 로그인 성공 시 로그인 처리
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { isValid },
+  } = useForm<FormData>();
+  const [message, setMessage] = useState('');
+
+  const onSubmit = async (data: FormData) => {
+    try {
+      const { password } = data;
+      const response = await (
+        await fetch('/api/auth/signin', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+        })
+      ).json();
+      if (response.error) {
+        setMessage('비밀번호가 일치하지 않습니다.');
+        return;
+      }
+
+      // 로그인 성공
+      console.log(response.user);
+    } catch (e) {
+      setMessage('에러가 발생하였습니다. 잠시 후 다시 시도해주세요.');
+    }
   };
   return (
     <div className={styles.wrapper}>
@@ -20,17 +57,31 @@ export const EmailLogin = ({ onPrevPage, email }: Props) => {
           <div className={styles.center}>이메일로 로그인</div>
           <div className={styles.right}></div>
         </div>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className={styles.inputBox}>
             <label htmlFor="password">비밀번호</label>
             <input
-              id="password"
+              className={message ? styles.inValid : styles.focusInput}
               type="password"
               placeholder="비밀번호를 입력해주세요."
+              {...register('password', {
+                required: true,
+                minLength: 2,
+                onChange: () => {
+                  setMessage('');
+                },
+              })}
             />
+            {message && <p className={styles.errorMessage}>{message}</p>}
           </div>
 
-          <button>다음</button>
+          <button
+            type="submit"
+            className={isValid ? styles.validButton : ''}
+            disabled={!isValid}
+          >
+            다음
+          </button>
         </form>
       </div>
     </div>
--- src/app/login/components/signup.tsx
+++ src/app/login/components/signup.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { IoChevronBackOutline } from 'react-icons/io5';
 import styles from './signup.module.scss';
 
@@ -6,7 +8,61 @@ interface Props {
   email: string;
 }
 
+interface FormData {
+  name: string;
+  password: string;
+  checkPassword: string;
+}
+
+const passwordRegex =
+  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
+
 export const Signup = ({ onPrevPage, email }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { isValid },
+  } = useForm<FormData>();
+
+  const [message, setMessage] = useState('');
+
+  const onSubmit = async (data: FormData) => {
+    try {
+      const { password, name } = data;
+      const response = await (
+        await fetch('/api/auth/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email,
+            password,
+            name,
+          }),
+        })
+      ).json();
+      if (response.error) {
+        setMessage(response.error);
+        return;
+      }
+      // 가입 성공
+      console.log(response.user);
+    } catch (e) {
+      setMessage('에러가 발생하였습니다. 잠시 후 다시 시도해주세요.');
+    }
+  };
+  const password = watch('password');
+  const passwordValid = passwordRegex.test(password);
+  const passwordClassName = passwordValid ? styles.valid : styles.inValid;
+
+  const checkPassword = watch('checkPassword');
+  const checkPasswordValid = password === checkPassword;
+  const checkPasswordClassName = checkPasswordValid
+    ? styles.valid
+    : styles.inValid;
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
@@ -17,7 +73,7 @@ export const Signup = ({ onPrevPage, email }: Props) => {
           <div className={styles.center}>회원가입</div>
           <div className={styles.right}></div>
         </div>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className={styles.inputBox}>
             <label htmlFor="email">이메일</label>
             <input id="email" type="email" value={email} disabled={true} />
@@ -25,28 +81,70 @@ export const Signup = ({ onPrevPage, email }: Props) => {
 
           <div className={styles.inputBox}>
             <label htmlFor="name">이름</label>
-            <input id="name" type="text" placeholder="이름을 입력해주세요." />
+            <input
+              className={styles.focusInput}
+              type="text"
+              placeholder="이름을 입력해주세요."
+              {...register('name', {
+                required: true,
+                minLength: 2,
+                maxLength: 20,
+              })}
+            />
           </div>
 
           <div className={styles.inputBox}>
             <label htmlFor="password">비밀번호</label>
             <input
-              id="password"
+              className={password ? passwordClassName : styles.focusInput}
               type="password"
               placeholder="비밀번호를 입력해주세요."
+              {...register('password', {
+                required: true,
+              })}
             />
+            {password && !passwordValid && (
+              <p className={styles.errorMessage}>
+                올바르지 않은 비밀번호입니다.
+              </p>
+            )}
             <input
-              id="checkPassword"
+              className={
+                checkPassword ? checkPasswordClassName : styles.focusInput
+              }
               type="password"
               placeholder="비밀번호를 다시 한번 입력해주세요."
+              {...register('checkPassword', {
+                required: true,
+                pattern: passwordRegex,
+              })}
             />
-            <p>
-              영문 대소문자, 숫자, 특수문자를 3가지 이상으로 조합해 8자 이상
-              16자 이하로 입력해주세요.
+            {checkPassword && !checkPasswordValid && (
+              <p className={styles.errorMessage}>
+                비밀번호가 서로 일치하지 않습니다.
+              </p>
+            )}
+            {password &&
+              passwordValid &&
+              checkPassword &&
+              checkPasswordValid && (
+                <p className={styles.successMessage}>
+                  사용 가능한 비밀번호입니다.
+                </p>
+              )}
+            <p className={styles.helpText}>
+              영문 대소문자, 숫자, 특수문자를 3가지 이상으로 조합해 8자 이상으로
+              입력해주세요.
             </p>
           </div>
 
-          <button>가입하기</button>
+          <button
+            type="submit"
+            className={isValid ? styles.validButton : ''}
+            disabled={!isValid}
+          >
+            가입하기
+          </button>
         </form>
       </div>
     </div>
