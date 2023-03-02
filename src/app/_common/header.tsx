7d637e8cd32f42366a5842523634a06699ad8a5f
woo3145 <lcwoo3145@gmail.com>
2023-03-02 12:22:18 +0900

    Work - 게시글 작성 (client)

--- src/app/_common/header.tsx
+++ src/app/_common/header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
