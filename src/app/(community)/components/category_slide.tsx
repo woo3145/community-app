ae59f9d410d01cf95cd566912188c5bf9bfbe8b6
woo3145 <lcwoo3145@gmail.com>
2023-03-13 23:17:27 +0900

    Work - Category Slide(3) Category 변경 시 스크롤 이동, More 컨테이너 추가

--- src/app/(community)/components/category_slide.tsx
+++ src/app/(community)/components/category_slide.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { CategoryButton } from '@/app/_common/category_button';
 import { useTags } from '@/hooks/useTags';
 import { useEffect, useRef, useState } from 'react';
@@ -14,9 +16,14 @@ export const CategorySlide = () => {
   const [categoryId, setCategoryId] = useState<number>();
   const { tags, subTags, isLoading, isError } = useTags();
 
+  const [leftVisible, setLeftVisible] = useState(false); // 슬라이드 왼쪽 버튼
+  const [rightVisible, setRightVisible] = useState(true); // 슬라이드 오른쪽 버튼
+  const [moreVisible, setMoreVisible] = useState(false); // 카테고리 펼치기 버튼
+
+  // 첫 로딩시 현재 페이지의 param으로 categoryId 설정
   useEffect(() => {
     if (typeof window === 'undefined') return;
-    if (categoryId == null) {
+    if (categoryId === undefined) {
       const pathname = window.location.pathname.slice(1);
       if (pathname === '') {
         setCategoryId(-1);
@@ -28,41 +35,96 @@ export const CategorySlide = () => {
     }
   }, [categoryId]);
 
+  // categoryId가 바뀌면 해당 카테고리의 아이템의 위치로 스크롤링
+  useEffect(() => {
+    if (categoryId === undefined) return;
+    if (!scrollRef.current) return;
+
+    const target = scrollRef.current.querySelector(
+      `#categoryButton_${categoryId}`
+    ) as HTMLElement;
+
+    // target의 위치에서 박스의 절반크기만큼 이동하여 중간으로 위치하도록함
+    const position = target.offsetLeft - scrollRef.current.offsetWidth / 2;
+
+    scrollRef.current.scrollTo({
+      left: position,
+    });
+    // 왼쪽 끝 도달 시 왼쪽버튼 제거
+    if (position <= 0) {
+      setLeftVisible(false);
+    } else {
+      setLeftVisible(true);
+    }
+
+    // 오른쪽 끝 도달 (최대길이 - 박스길이) 시 오른쪽버튼 제거
+    if (
+      scrollRef.current.scrollWidth - scrollRef.current.offsetWidth <=
+      position
+    ) {
+      setRightVisible(false);
+    } else {
+      setRightVisible(true);
+    }
+  }, [categoryId]);
+
+  // 링크 이동
   const onClickCategory = (categoryId: number) => {
     setCategoryId(categoryId);
+    setMoreVisible(false);
   };
 
+  // 슬라이드 왼쪽이동
   const onClickLeft = () => {
     if (!scrollRef.current) return;
-    const curLeft = scrollRef.current.scrollLeft;
-    if (curLeft <= 200) {
+
+    const pos = scrollRef.current.scrollLeft - 200; // 이동 할 위치
+    if (pos <= 0) {
       scrollRef.current.scrollTo({ left: 0 });
+      setLeftVisible(false);
     } else {
-      scrollRef.current.scrollTo({ left: curLeft - 200 });
+      scrollRef.current.scrollTo({ left: pos });
+      setRightVisible(true);
     }
   };
+  // 슬라이드 오른쪽 이동
   const onClickRight = () => {
     if (!scrollRef.current) return;
-    const curLeft = scrollRef.current.scrollLeft;
-    const maxWidth = scrollRef.current.scrollWidth;
-    if (maxWidth < curLeft + 200) {
+
+    const maxWidth = scrollRef.current.scrollWidth; // 스크롤 총 길이
+    const boxWidth = scrollRef.current.offsetWidth; // 박스 길이
+
+    const pos = scrollRef.current.scrollLeft + 200; // 이동 할 위치
+
+    if (maxWidth - boxWidth <= pos) {
       scrollRef.current.scrollTo({ left: maxWidth });
+      setRightVisible(false);
     } else {
-      scrollRef.current.scrollTo({ left: curLeft + 200 });
+      scrollRef.current.scrollTo({ left: pos });
+      setLeftVisible(true);
     }
   };
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <div className={styles.flexBox}>
           <div className={styles.category_list} ref={scrollRef}>
             <CategoryButton
+              id={`categoryButton_-1`}
+              key={-1}
               title="추천"
               href=""
               onClick={() => onClickCategory(-1)}
               selected={categoryId === -1}
             />
             <CategoryButton
+              id={`categoryButton_0`}
+              key={0}
               title="전체"
               href="/all"
               onClick={() => onClickCategory(0)}
@@ -71,6 +133,7 @@ export const CategorySlide = () => {
             {subTags?.map((category) => {
               return (
                 <CategoryButton
+                  id={`categoryButton_${category.id}`}
                   key={category.id}
                   title={category.title}
                   href={`/${category.id}`}
@@ -81,17 +144,51 @@ export const CategorySlide = () => {
             })}
           </div>
 
-          <div className={styles.leftButton} onClick={onClickLeft}>
-            <IoChevronBack />
-          </div>
-          <div className={styles.rightButton} onClick={onClickRight}>
-            <IoChevronForwardSharp />
-          </div>
+          {leftVisible && (
+            <div className={styles.leftButton} onClick={onClickLeft}>
+              <IoChevronBack />
+            </div>
+          )}
+          {rightVisible && (
+            <div className={styles.rightButton} onClick={onClickRight}>
+              <IoChevronForwardSharp />
+            </div>
+          )}
         </div>
-        <div className={styles.moreButton}>
+        <div
+          className={styles.moreButton}
+          onClick={() => setMoreVisible(!moreVisible)}
+        >
           <IoEllipsisHorizontal />
         </div>
       </div>
+      {moreVisible && (
+        <div className={styles.moreContainer}>
+          <CategoryButton
+            title="추천"
+            href=""
+            onClick={() => onClickCategory(-1)}
+            selected={categoryId === -1}
+          />
+          <CategoryButton
+            title="전체"
+            href="/all"
+            onClick={() => onClickCategory(0)}
+            selected={categoryId === 0}
+          />
+          {subTags?.map((category) => {
+            return (
+              <CategoryButton
+                key={category.id}
+                title={category.title}
+                href={`/${category.id}`}
+                onClick={() => onClickCategory(category.id)}
+                selected={categoryId === category.id}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
