e0d2a4b7b213e6bab72ad85a1cd094bb729a51e4
woo3145 <lcwoo3145@gmail.com>
2023-05-05 20:54:22 +0900

    Work - EmptyPostContainer 생성, Test - EmptyPostContainer 확인

--- cypress/e2e/main.spec.ts
+++ cypress/e2e/main.spec.ts
@@ -7,18 +7,27 @@ describe('DB 초기화 테스트', () => {
     cy.visit('/');
   });
 
-  it('성공적으로 DB가 초기화되고 Seed(default Tags)가 추가됨', () => {
-    // Subtags + 전체 + 추천 - ParentTags
-    const subTagLength = DEFAULT_TAG.length + 2 - 3;
+  // Subtags + 전체 + 추천 - ParentTags
+  const subTagLength = DEFAULT_TAG.length + 2 - 3;
 
+  it('성공적으로 DB가 초기화되고 Seed(default Tags)가 추가됨', () => {
     // 태그 개수 확인
     cy.dataCy('category-list').children().should('have.length', subTagLength);
 
-    // more 버튼
+    // '카테고리 more 버튼이 정상작동함'
     cy.dataCy('category-list-more').should('not.exist');
+
     cy.dataCy('more-button').click();
     cy.dataCy('category-list-more')
       .children()
       .should('have.length', subTagLength);
+
+    cy.dataCy('more-button').click();
+    cy.dataCy('category-list-more').should('not.exist');
+
+    // EmptyPostContainer 가 표시되어야함
+    cy.dataCy('empty-post-container').should('exist');
+    cy.dataCy('category_5').click();
+    cy.dataCy('empty-post-container').should('exist');
   });
 });
--- src/app/_components/organisms/PostList/index.tsx
+++ src/app/_components/organisms/PostList/index.tsx
@@ -8,19 +8,38 @@ interface Props {
   category: string;
 }
 
+const EmptyPostContainer = () => {
+  return (
+    <div className={styles.emptyPostContainer} data-cy={'empty-post-container'}>
+      <div className={styles.emptyIcon}>✏️</div>
+      <div className={styles.emptyPostText}>
+        <p>등록된 게시물이 없습니다.</p>
+        <p>첫번째 게시물을 작성해 보세요!</p>
+      </div>
+    </div>
+  );
+};
+
 export default function PostList({ category }: Props) {
   const { data, isLoading, bottomRef, isReachedEnd } = usePosts(category);
+  console.log(data);
   return (
     <div className={styles.container}>
-      {isLoading
-        ? [1, 2, 3, 4].map((i, idx) => {
-            return <PostItem isLoading={isLoading} key={idx} />;
+      {isLoading &&
+        data.length === 0 &&
+        [1, 2, 3, 4].map((i, idx) => {
+          return <PostItem isLoading={isLoading} key={idx} />;
+        })}
+      {!isLoading && data.length !== 0 && data[0].data.length === 0 && (
+        <EmptyPostContainer />
+      )}
+      {data.length !== 0 &&
+        data.map((page) =>
+          page.data.map((post) => {
+            return <PostItem isLoading={false} key={post.id} post={post} />;
           })
-        : data.map((page) =>
-            page.data.map((post) => {
-              return <PostItem isLoading={false} key={post.id} post={post} />;
-            })
-          )}
+        )}
+
       {isLoading ? (
         <PostItem isLoading={isLoading} />
       ) : (
