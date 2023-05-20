d60a39c7454bd11078d26005fdbf30d12b5333c1
woo3145 <lcwoo3145@gmail.com>
2023-05-21 04:28:37 +0900

    🎨 프로필 수정 API 엔드포인트 변경

---
+++ src/pages/api/my/profile.ts
@@ -0,0 +1,87 @@
+import { withErrorHandling } from '@/libs/server/errorHandler';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import client from '@/libs/server/prismaClient';
+import { Session, getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
+import {
+  MethodNotAllowedError,
+  NotFoundError,
+  UnauthorizedError,
+} from '@/libs/server/customErrors';
+import { getProfileByUserId } from '@/libs/prisma/profile';
+
+interface EditProfileBody {
+  nameType: boolean;
+  nickname: string;
+  description: string;
+  avatar: string;
+}
+
+const allowedMethods = ['PUT'];
+
+async function handlePUT(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  session: Session
+) {
+  const { nameType, nickname, description, avatar } =
+    req.body as EditProfileBody;
+
+  const profile = await getProfileByUserId(session.user.id);
+  if (!profile) {
+    throw new NotFoundError('profile');
+  }
+  let isChanged = false;
+  const updatedProfile = {
+    nickname: profile.nickname,
+    description: profile.description,
+    nameType: profile.nameType, // true: 닉네임, false: 이름
+    avatar: profile.avatar,
+  };
+  if (nameType !== profile.nameType) {
+    isChanged = true;
+    updatedProfile.nameType = nameType;
+  }
+  if (nameType === true && nickname !== profile.nickname) {
+    isChanged = true;
+    updatedProfile.nickname = nickname;
+  }
+  if (description !== profile.description) {
+    isChanged = true;
+    updatedProfile.description = description;
+  }
+  if (avatar !== profile.avatar) {
+    isChanged = true;
+    updatedProfile.avatar = avatar;
+  }
+
+  if (isChanged) {
+    await client.profile.update({
+      where: {
+        id: profile.id,
+      },
+      data: {
+        ...updatedProfile,
+      },
+    });
+  }
+  return res.status(200).json({ message: 'successful' });
+}
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (!allowedMethods.includes(req.method!)) {
+    throw new MethodNotAllowedError();
+  }
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) {
+    throw new UnauthorizedError();
+  }
+
+  // 내 프로필 수정
+  if (req.method === 'PUT') {
+    return handlePUT(req, res, session);
+  }
+}
+
+export default withErrorHandling(handler);
