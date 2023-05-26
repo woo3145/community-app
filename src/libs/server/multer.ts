cb5a762ae021a293f11c8b31352031afb67d231d
woo3145 <lcwoo3145@gmail.com>
2023-05-26 16:06:15 +0900

    🐛 Vercel 배포 버그 - 예약된 환경변수 이름 수정

--- src/libs/server/multer.ts
+++ src/libs/server/multer.ts
@@ -4,8 +4,8 @@ import { S3Client } from '@aws-sdk/client-s3';
 
 const s3 = new S3Client({
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY as string,
-    secretAccessKey: process.env.AWS_SECRET_KEY as string,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
   },
   region: process.env.AWS_REGION,
 });
