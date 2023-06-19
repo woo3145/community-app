058f5a05f6f797694a1a84243f09a3e2040062c0
woo3145 <lcwoo3145@gmail.com>
2023-06-19 19:42:49 +0900

    🐛 유저정보 로딩 중 글쓰기 버튼 막기

--- src/app/(main)/_components/WriteButtonContainer.tsx
+++ src/app/(main)/_components/WriteButtonContainer.tsx
@@ -10,7 +10,7 @@ import { MyProfileModifyModal } from '@/app/_components/modals/MyProfileModifyMo
 export const WriteButtonContainer = () => {
   const [modalIsOpen, setIsOpen] = useState<boolean>(false);
   const router = useRouter();
-  const { me } = useMe();
+  const { me, isLoading } = useMe();
   const openModal = () => {
     setIsOpen(true);
   };
@@ -18,6 +18,7 @@ export const WriteButtonContainer = () => {
     setIsOpen(false);
   };
   const onClick = () => {
+    if (isLoading) return;
     if (me && me.profile.nameType === false) {
       if (
         confirm('해당 이름으로 게시글과 댓글이 등록됩니다. 계속하시겠어요?')
@@ -33,24 +34,32 @@ export const WriteButtonContainer = () => {
   };
 
   return (
-    <div className="flex items-center justify-center p-8 border-t-0 rounded-none card">
-      <div className="mr-4 shrink-0">
-        <Avatar src={me?.profile.avatar || ''} />
+    <div>
+      {/* Desktop */}
+      <div className="items-center justify-center hidden p-8 border-t-0 rounded-none xl:flex card">
+        <div className="mr-4 shrink-0">
+          <Avatar src={me?.profile.avatar || ''} />
+        </div>
+        <div
+          onClick={onClick}
+          className="flex items-center justify-between w-full px-5 py-4 border border-gray-200 border-solid rounded-md cursor-pointer"
+          data-cy={'postWrite-button'}
+        >
+          <span>커리어와 라이프스타일에 대해 자유롭게 이야기 해주세요!</span>
+          <HiOutlinePencil size={24} />
+        </div>
       </div>
+      {/* Mobile */}
       <div
         onClick={onClick}
-        className="flex items-center justify-between w-full px-5 py-4 border border-gray-200 border-solid rounded-md cursor-pointer"
         data-cy={'postWrite-button'}
+        className="fixed z-30 flex items-center justify-center w-12 h-12 text-white rounded-full cursor-pointer bottom-8 right-8 bg-primary"
       >
-        <span>커리어와 라이프스타일에 대해 자유롭게 이야기 해주세요!</span>
         <HiOutlinePencil size={24} />
       </div>
+
       {modalIsOpen && me && (
-        <MyProfileModifyModal
-          modalIsOpen={modalIsOpen}
-          closeModal={closeModal}
-          profile={me.profile}
-        />
+        <MyProfileModifyModal closeModal={closeModal} profile={me.profile} />
       )}
     </div>
   );
